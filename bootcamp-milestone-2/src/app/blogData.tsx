1a2c5e912d2399454041216b56a00ee62c58bf56
sipactivism <discoet9162@gmail.com>
2025-10-31 05:16:09 -0700

    feat: move from app/ to src/app/

---
+++ bootcamp-milestone-2/src/app/blogData.tsx
@@ -0,0 +1,30 @@
+export interface Blog {
+  title: string;
+  date: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  slug: string;
+}
+
+const blogEntries: Blog[] = [
+  {
+    title: "Hello world",
+    date: "10-16-2025",
+    description: "My first blog post",
+    image: "welcome.jpg",
+    imageAlt: "Green whiteboard with the text 'welcome' and a smiley face",
+    slug: "blogs/first-post.html",
+  },
+  {
+    title: "Subway",
+    date: "10-16-2025",
+    description: "A rant about my favorite restaurant on campus",
+    image: "subwaycookiebag.jpg",
+    imageAlt:
+      "Subway cookie bag with the text 'lookie lookie, I've got a cookie'",
+    slug: "blogs/subway.html",
+  },
+];
+
+export default blogEntries;
